893de79b0762caaca5dfa91bfb06abfa4f8f76b2
hippojamba <oliver.sabler@gmail.com>
2021-06-10 11:15:55 +0200

    disable HAA warnings in middleware

--- src/Upnodo.Api/Middleware/Exceptions/ExceptionMiddleware.cs
+++ src/Upnodo.Api/Middleware/Exceptions/ExceptionMiddleware.cs
@@ -7,7 +7,7 @@ using Polly;
 using Polly.CircuitBreaker;
 using Upnodo.BuildingBlocks.Application.Exceptions;
 
-#pragma warning disable HAA0301 HAA0302 HAA0303
+#pragma warning disable HAA0301, HAA0302, HAA0303
 namespace Upnodo.Api.Middleware.Exceptions
 {
     public class ExceptionMiddleware
@@ -65,5 +65,5 @@ namespace Upnodo.Api.Middleware.Exceptions
                         .ToString());
         }
     }
-#pragma warning restore CA2200 // Rethrow to preserve stack details
 }
+#pragma warning restore HAA0301, HAA0302, HAA0303
